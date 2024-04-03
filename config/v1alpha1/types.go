9990f21f5c04d3f6b398d6361e4a11baedf50406
Damian Peckett <damian@pecke.tt>
2024-04-03 11:56:40 +0200

    refactor: add omitempty tags to optional config fields

--- config/v1alpha1/types.go
+++ config/v1alpha1/types.go
@@ -22,10 +22,10 @@ const ApiVersion = "noisysockets.github.com/v1alpha1"
 type Config struct {
 	types.TypeMeta `yaml:",inline" mapstructure:",squash"`
 	// Name is the optional hostname of this peer.
-	Name string `yaml:"name" mapstructure:"name"`
+	Name string `yaml:"name,omitempty" mapstructure:"name,omitempty"`
 	// ListenPort is an optional port on which to listen for incoming packets.
 	// If not specified, one will be chosen randomly.
-	ListenPort uint16 `yaml:"listenPort" mapstructure:"listenPort"`
+	ListenPort uint16 `yaml:"listenPort,omitempty" mapstructure:"listenPort,omitempty"`
 	// PrivateKey is the private key for this peer.
 	PrivateKey string `yaml:"privateKey" mapstructure:"privateKey"`
 	// IPs is a list of IP addresses assigned to this peer.
@@ -33,23 +33,23 @@ type Config struct {
 	// DNSServers is an optional list of DNS servers to use for host resolution.
 	DNSServers []string `yaml:"dnsServers,omitempty" mapstructure:"dnsServers,omitempty"`
 	// Peers is a list of known peers to which we can send and receive packets.
-	Peers []PeerConfig `yaml:"peers" mapstructure:"peers"`
+	Peers []PeerConfig `yaml:"peers,omitempty" mapstructure:"peers,omitempty"`
 }
 
 // PeerConfig is the configuration for a known wireguard peer.
 type PeerConfig struct {
 	// Name is the optional hostname of the peer.
-	Name string `yaml:"name" mapstructure:"name"`
+	Name string `yaml:"name,omitempty" mapstructure:"name,omitempty"`
 	// PublicKey is the public key of the peer.
 	PublicKey string `yaml:"publicKey" mapstructure:"publicKey"`
 	// Endpoint is an optional endpoint to which the peer's packets should be sent.
 	// If not specified, the peers endpoint will be determined from received packets.
-	Endpoint string `yaml:"endpoint" mapstructure:"endpoint"`
+	Endpoint string `yaml:"endpoint,omitempty" mapstructure:"endpoint,omitempty"`
 	// IPs is a list of IP addresses assigned to the peer, this is optional for gateways.
 	// Traffic with a source IP not in this list will be dropped.
 	IPs []string `yaml:"ips,omitempty" mapstructure:"ips,omitempty"`
 	// DefaultGateway indicates this peer should be used as the default gateway for traffic.
-	DefaultGateway bool `yaml:"defaultGateway" mapstructure:"defaultGateway"`
+	DefaultGateway bool `yaml:"defaultGateway,omitempty" mapstructure:"defaultGateway,omitempty"`
 }
 
 func (c Config) GetKind() string {
